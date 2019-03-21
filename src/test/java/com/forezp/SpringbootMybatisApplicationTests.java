a6accf7b217aa802efaf2222b1a13c56b061e959
Jasonmin <313410158@qq.com>
2019-03-21 14:46:07 +0800

    redis

--- src/test/java/com/forezp/SpringbootMybatisApplicationTests.java
+++ src/test/java/com/forezp/SpringbootMybatisApplicationTests.java
@@ -1,7 +1,10 @@
 package com.forezp;
 
+import com.forezp.dao.RedisDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootMybatisApplicationTests {
 
+//	public  static  Logger logger = LoggerFactory.getLogger(SpringbootMybatisApplicationTests.class);
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	RedisDao redisDao;
+	@Test
+	public void testRedis() {
+
+		System.out.println("==nothing");
+
+		redisDao.setKey("user","zhouqiang");
+		redisDao.setKey("friend","zhoujie");
+
+		System.out.println(redisDao.getValue("user"));
+
+		System.out.println(redisDao.getValue("friend"));
 	}
 
 }
